0cb2cf4eb8ba725fd6661ef6359a2696567c138f
Alon Zakai <alonzakai@gmail.com>
2013-09-07 12:42:12 -0700

    docs for web build

--- doit.sh
+++ doit.sh
@@ -1,5 +1,9 @@
-./configure --disable-i386-asm --disable-gl --disable-cpu-opt
+### native
+#./configure --disable-i386-asm --disable-gl --disable-cpu-opt
 # make
 # ./src/prboom -nofullscreen -nosound
 
+### web
+#~/Dev/emscripten/emconfigure ./configure --disable-i386-asm --disable-gl --disable-cpu-opt
+#make
 
