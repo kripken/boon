3ad7128f4dc93a439b8fe220b49ca9fbe855e76a
Alon Zakai <alonzakai@gmail.com>
2013-09-07 13:39:47 -0700

    add commandline args to html

--- doit.sh
+++ doit.sh
@@ -7,5 +7,5 @@
 #~/Dev/emscripten/emconfigure ./configure --disable-i386-asm --disable-gl --disable-cpu-opt
 #make
 #mv src/prboom src/prboom.bc
-#~/Dev/emscripten/emcc src/prboom.bc -o boon.html --preload-file prboom.wad --preload-file doom.wad -s TOTAL_MEMORY=134217728 -O2 -g
+#~/Dev/emscripten/emcc src/prboom.bc -o boon.html --preload-file prboom.wad --preload-file doom.wad -s TOTAL_MEMORY=134217728 -O2 -g --pre-js pre.js
 
---
+++ pre.js
@@ -0,0 +1,3 @@
+
+Module.arguments = ['-nofullscreen', '-nosound'];
+
