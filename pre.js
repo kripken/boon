e9fbd9d3b01a73bb8087c35b9ea6101ee17626f1
Alon Zakai <alonzakai@gmail.com>
2013-09-07 17:12:15 -0700

    add fps

--- pre.js
+++ pre.js
@@ -2,3 +2,27 @@
 Module.arguments = ['-nofullscreen', '-nosound'];
 Module.screenIsReadOnly = true;
 
+(function() {
+  var start = 0;
+  var mean = 0;
+  var reportTime = Date.now();
+
+  Module.preMainLoop = function() {
+    start = Date.now();
+  };
+  Module.postMainLoop = function() {
+    var now = Date.now();
+    var time = now - start;
+    if (mean == 0) {
+      mean = time;
+    } else {
+      mean = 0.1*time + 0.9*mean;
+    }
+    if (now > reportTime + 1000) {
+      var fps = 1000/mean;
+      console.log('fps: ' + Math.round(fps));
+      reportTime = now;
+    }
+  };
+})();
+
