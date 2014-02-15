1040cb872b7b6ab3f40f92deb45d9758417e3f07
Alon Zakai <alonzakai@gmail.com>
2014-02-15 14:54:35 -0800

    update build command

--- doit.sh
+++ doit.sh
@@ -7,5 +7,5 @@
 #~/Dev/emscripten/emconfigure ./configure --disable-i386-asm --disable-gl --disable-cpu-opt --without-net --with-sdl-prefix=/home/alon/Dev/emscripten/system
 make
 mv src/prboom src/prboom.bc
-~/Dev/emscripten/emcc src/prboom.bc -o boon.html --preload-file prboom.wad --preload-file doom.wad -s TOTAL_MEMORY=134217728 -O2 --pre-js pre.js --proxy-to-worker
+EMCC_FAST_COMPILER=1 ~/Dev/emscripten/emcc src/prboom.bc -o boon.html --preload-file prboom.wad --preload-file doom.wad -s TOTAL_MEMORY=134217728 -O3 --pre-js pre.js --proxy-to-worker
 
