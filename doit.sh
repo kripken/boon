c8926e562cb241868a4c799416f9639de939bde0
Alon Zakai <alonzakai@gmail.com>
2014-11-07 11:36:06 -0800

    update make stuff

--- doit.sh
+++ doit.sh
@@ -4,8 +4,8 @@
 # ./src/prboom -nofullscreen -nosound
 
 ### web
-#~/Dev/emscripten/emconfigure ./configure --disable-i386-asm --disable-gl --disable-cpu-opt --without-net --with-sdl-prefix=/home/alon/Dev/emscripten/system
-make
+#EMCONFIGURE_JS=1 ~/Dev/emscripten/emconfigure ./configure --disable-i386-asm --disable-gl --disable-cpu-opt --without-net --with-sdl-prefix=/home/alon/Dev/emscripten/system
+make -j 2
 mv src/prboom src/prboom.bc
-EMCC_FAST_COMPILER=1 ~/Dev/emscripten/emcc src/prboom.bc -o boon.html --preload-file prboom.wad --preload-file doom.wad -s TOTAL_MEMORY=134217728 -O3 --pre-js pre.js --proxy-to-worker
+~/Dev/emscripten/emcc src/prboom.bc -o boon.html --preload-file prboom.wad --preload-file doom.wad -s TOTAL_MEMORY=134217728 -O3 --pre-js pre.js
 
